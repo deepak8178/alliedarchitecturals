57e3d79bc8d7b22f110da344315fcf5823ffd7a8
deepak8178 <alliedarchitecturals@gmail.com>
2023-09-25 05:27:59 +0530

    UPDATED

--- app.js
+++ app.js
@@ -13,18 +13,19 @@ const app = express();
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD,
+    "<password>",
+    process.env.DATABASE_PASSWORD
 );
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connection successful!!"));
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB connection successful!!"))
+    .catch((err) => console.error("DB connection error:", err));
 
 // Setting template engine
 app.set("view engine", "pug");
@@ -33,13 +34,13 @@ app.set("views", path.join(__dirname, "views"));
 // Serving Static Files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Body parser, reading data from body into request.body
+// Body parser, reading data from the body into request.body
 app.use(express.json({ limit: "10kb" }));
 
 // Test middleware
 app.use((request, response, next) => {
-  request.requestTime = new Date().toISOString();
-  next();
+    request.requestTime = new Date().toISOString();
+    next();
 });
 
 // ROUTES
@@ -48,14 +49,14 @@ app.use("/api/blogs", blogRouter);
 app.use("/api/services", serviceRouter);
 
 app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`), 404);
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 // Server
-const port = 3000;
-const host = process.env.HOST;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port} and on server ${host}...`);
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+const server = app.listen(port, host, () => {
+    console.log(`App running on port ${port} and on host ${host}...`);
 });
 
 module.exports = app;
